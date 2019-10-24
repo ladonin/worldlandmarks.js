a5cb06331a5f4ee3f9dfca54d31f9053e2b7204d
Ladonin Alexander <ladonin85@mail.ru>
2019-10-24 08:26:52 -0400

    fnal fixes 5

--- modules/common/base_functions/BaseFunctions.js
+++
@@ -1,8 +0,0 @@
-/*import ErrorHandler from 'src/modules/error_handler/ErrorHandler';
-import Messages from 'src/settings/Messages';
-import ErrorCodes from 'src/settings/ErrorCodes';
-const _lang = require('lodash/util');
-const _string = require('lodash/string');
-
-
-*/
